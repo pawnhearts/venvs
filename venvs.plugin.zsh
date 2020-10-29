a70f6693342b2c6061cefdeef752b72aba58bb2c
ph <robotnaoborot@gmail.com>
2020-10-29 20:13:14 +0300

    init

--- venvs.plugin.zsh
+++ venvs.plugin.zsh
@@ -1,7 +1,7 @@
 export AUTO_VENV_PATH=""
 export PROJECT_ROOT=""
 export USE_DOTENV=""
-export OLD_ENV=``
+export OLD_ENV=''
 
 [[ "$VENVS_DIR" ]] || export VENVS_DIR="~/.virtualenvs"
 
@@ -12,7 +12,6 @@ function check_venv() {
         [[ -e "$VENVS_DIR/$(basename $(pwd))" ]] && export AUTO_VENV_PATH="$VENVS_DIR/$(basename $(pwd))"
         source "$AUTO_VENV_PATH/bin/activate" && export PROJECT_ROOT="$(pwd)" || export PROJECT_ROOT=""
         if [[ "$USE_DOTENV" ]] && [[ -f ".env" ]]; then
-            export OLD_ENV=`env`
             eval `cat .env`
         fi
 
@@ -21,23 +20,21 @@ function check_venv() {
             which deactivate >/dev/null && deactivate
             export AUTO_VENV_PATH=""
             export PROJECT_ROOT=""
-            [[ "$OLD_ENV" ]] && eval $OLD_ENV
-            export OLD_ENV=``
         fi
     fi
 }
 
-function enable_autoswitch_virtualenv() {
+function enable_venvs() {
     autoload -Uz add-zsh-hook
-    disable_autoswitch_virtualenv
+    disable_venvs
     add-zsh-hook chpwd check_venv
 }
 
 
-function disable_autoswitch_virtualenv() {
+function disable_venvs() {
     add-zsh-hook -D chpwd check_venv
 }
 
 
-enable_autoswitch_virtualenv
+enable_venvs
 check_venv
